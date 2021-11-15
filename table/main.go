c5c62a5d6976da4970c21b95b1011582289f7767
Ryoji <ryoji.kodakari@gmail.com>
2021-11-16 08:04:00 +0900

    trie table

--- table/main.go
+++ table/main.go
@@ -26,58 +26,70 @@ func main() {
 
 	bbcNode := t.node("bbc")
 	fmt.Println(prettyPrint(bbcNode))
-	fmt.Println(bbcNode.ids())
+	fmt.Println(t.ids(bbcNode))
 
 	cnodes := t.node("c")
 	fmt.Println(prettyPrint(cnodes))
-	fmt.Println(cnodes.ids())
+	fmt.Println(t.ids(cnodes))
 }
 
 type trie struct {
-	Children   []trie `json:",omitempty"`
-	Id         *int   `json:",omitempty"`
-	Debug      string `json:",omitempty"`
+	Nodes      [][]node
 	atoi       func(rune) int
 	nOfLetters int
 }
 
+type node struct {
+	OffspringRow *int   `json:",omitempty"`
+	Id           *int   `json:",omitempty"`
+	Debug        string `json:",omitempty"`
+}
+
 func newTrie(atoi func(rune) int, nOfLetters int) *trie {
 	return &trie{
-		Children:   make([]trie, nOfLetters),
-		Id:         nil,
-		Debug:      string('/'),
+		Nodes: [][]node{
+			make([]node, nOfLetters),
+		},
 		atoi:       atoi,
 		nOfLetters: nOfLetters,
 	}
 }
 
-func (t *trie) initialize(debug rune, rest []rune, id int, parent *trie) {
-	t.Debug = string(debug)
-	t.atoi = parent.atoi
-	t.nOfLetters = parent.nOfLetters
+func (n *node) initialize(debug rune, rest []rune, id int, t *trie) {
+	n.Debug = string(debug)
+	if n.OffspringRow == nil {
+		t.Nodes = append(t.Nodes, make([]node, t.nOfLetters))
+		row := len(t.Nodes) - 1
+		n.OffspringRow = &row
+	}
 	if len(rest) == 0 {
 		id := int(id)
-		t.Id = &id
+		n.Id = &id
 		return
 	}
-	t.addChars(rest, id)
+	n.addChars(rest, id, t)
 }
 
-func (t *trie) addChars(chars []rune, id int) {
+func (n *node) addChars(chars []rune, id int, t *trie) {
 	if len(chars) == 0 {
 		return
 	}
 	r := chars[0]
-	i := t.atoi(r)
-	if len(t.Children) != t.nOfLetters {
-		t.Children = make([]trie, t.nOfLetters)
-	}
-	child := &t.Children[i]
+	row := *n.OffspringRow
+	child := &t.Nodes[row][t.atoi(r)]
 	child.initialize(r, chars[1:], id, t)
 }
 
-func (t *trie) isPhantom() bool {
-	return len(t.Children) == 0 && t.Id == nil
+func (t *trie) addChars(chars []rune, id int) {
+	initialRow := 0
+	rootNode := node{
+		OffspringRow: &initialRow,
+	}
+	rootNode.addChars(chars, id, t)
+}
+
+func (n *node) isPhantom() bool {
+	return n.OffspringRow == nil && n.Id == nil
 }
 
 func (t *trie) addWord(word string, id int) {
